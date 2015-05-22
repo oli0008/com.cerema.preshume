558c35fa1869d5b9916fefc7b8d373b9c1fa6a3e
XXX <xxx@xxx.com>
2015-05-22 10:51:34 +0200

    Update# 11

--- src/Contents/Application/app/view/VMain.js
+++ src/Contents/Application/app/view/VMain.js
@@ -74,7 +74,7 @@ App.view.define('VMain', {
 						{
 							xtype: "button",		//xtype création d'un obj GUI
 							itemId: "clickme",
-							text: "xxOK",
+							text: "OK",
 							width: 100,
 							margin: {
 								left: 20,
