f5a83df5319d9d5a831e6729f47932303233def6
XXX <xxx@xxx.com>
2015-05-22 09:55:08 +0200

    Update# 3

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -29,7 +29,7 @@ App.controller.define('CMain', {
 		};			
 	},
 	clickme_onclick: function()
-/*
+
 	{
 		function async_addition(a,b,retour) {
 			retour(a+b);
@@ -47,22 +47,49 @@ App.controller.define('CMain', {
 			console.log('reponse= ' + reponse);	
 		});		//résultat==> reponse= 20 --- Ok
 		
-
+/*3
+			funtion grand2(reponse){
+				console.log('reponse1= ' + reponse);
+			};
+				calcul(0,function grand2(reponse));
+				console.log('reponse2= ' + reponse);		//résultat==> reponse= 20   ---NOK
+*/
 
 	},
 
-*/	
-
 	
+/*2
+	{
+		function async_addition(a,b,fonction_retour) {
+			fonction_retour(a+b);
+			// return a+b
+		}
 
-	
+		var total=0;
+		function calcul(i,cb) {
+			async_addition(3,i,function(e) {
+				total+=e;
+			
+				if (i<=3) {
+					console.log(total);
+					calcul(i+1,cb);	
+				} else cb(total);
+			});
+		};
+
+		calcul(0,function(reponse) {
+			console.log(reponse);	
+		});
+	},
+*/
+/*1	
 	{
 		// on charge le store avec une variable "recherche"
 		App.get('grid#grid1').getStore().getProxy().extraParams.recherche=App.get('textfield#text1').getValue();
 		// on rafraichit le store
 		App.get('grid#grid1').getStore().load();
 	},
-
+	*/
 	onLoad: function()
 	{
 		// form loaded	
