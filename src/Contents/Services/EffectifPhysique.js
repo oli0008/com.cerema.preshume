8d0bc7f43dc3ca7655c2ca624464f32da1c47fd8
XXX <xxx@xxx.com>
2015-05-22 14:44:38 +0200

    Update# 12

---
+++ src/Contents/Services/EffectifPhysique.js
@@ -0,0 +1,21 @@
+// class EffectifPhysique
+// class omneedia db = classe moteur d'abstraction de base de données
+
+EffectifPhysique = {
+	 cherche: function(t,fonction_retour) {
+		// 1er argument = error
+		// 2eme argument = response
+		var q=EffectifPhysique.using('db');  
+		//q.model
+		//q.post
+		//q.delete
+		//q.sql method
+		console.log(q.sql('get_agent',{RECHERCHE:'%' + t.recherche + '%'}));	//log apparait dans DOS box
+		console.log('*** oli ***');
+		q.model('bpclight',q.sql('get_agent',{RECHERCHE:'%' + t.recherche + '%'}),fonction_retour);
+		//q.model('bpclight','select nom,prenom from EffectifPhysique',fonction_retour);
+	}
+	
+};
+
+module.exports=EffectifPhysique;
---
+++ src/Contents/Services/sql/get_effectifPhysiquePresent.sql
@@ -0,0 +1,37 @@
+/* Effectifs physiques à l'instant présent */
+/* groupés par Unité & par grades */
+
+/* NOTE: la table agents représente les éffectifs à l'instant T. */
+
+
+select 
+	catgrad.LibCgr,
+	count(agents.kage), 
+	unites.LibUnic 
+from 
+	agents 
+	join unites 
+			on unites.Kuni=agents.Kuni 
+	join grades 
+			on grades.kgra=agents.kgra 
+	join catgrad 
+			on catgrad.Kcgr=grades.kcgr 
+	join etablissements
+			on etablissements.Kets = unites.Kets
+where 
+	agents.actif=1 
+	and 
+	agents.kgra<>66 
+	and 
+	agents.kgra<>67 
+	and
+	etablissements.archive = 0
+	and
+	unites.archive = 0
+	and 
+	etablissements.Kets = 1	/* LibEtsC est NULL pour Kets = 6, Kets = 1 == DTerMed */
+/*	and
+	unites.Kuni = 11 */ 	/*selecteur d'unité */
+group by 
+	unites.LibUnic,
+	grades.kcgr
