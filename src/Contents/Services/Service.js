536459809bc93ccf53ee61ffff76aaca9b83ce75
XXX <xxx@xxx.com>
2015-05-26 15:02:31 +0200

    Update# 130

--- src/Contents/Services/Service.js
+++ src/Contents/Services/Service.js
@@ -1,8 +1,8 @@
 Service={
 	get_Service: function(o,cb)
 	{
-	//	Service.using('db').model('bpclight','select kuni, LibSubc, LibSub from subdis where archive = 0 ',cb);
-		Service.using('db').model('bpclight','select kuni, LibSubc, LibSub from subdis where archive = 0 and kuni = 13',cb);
+		Service.using('db').model('bpclight','select kuni, LibSubc, LibSub from subdis where archive = 0 ',cb);
+	//	Service.using('db').model('bpclight','select kuni, LibSubc, LibSub from subdis where archive = 0 and kuni = 13',cb);
 		//		Unite.using('db').model('bpclight',q.sql('get_Unite'),cb); 
 	}
 };
