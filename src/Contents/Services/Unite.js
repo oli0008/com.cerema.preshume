3f7ed58093e87db7f58d1af4acea788b513f6e01
XXX <xxx@xxx.com>
2015-05-26 15:01:29 +0200

    Update# 129

--- src/Contents/Services/Unite.js
+++ src/Contents/Services/Unite.js
@@ -2,6 +2,7 @@ Unite={
 	get_Unite: function(o,cb)
 	{
 		Unite.using('db').model('bpclight','select kuni, LibUnic, LibUni from unites where archive = 0',cb);
+//		Unite.using('db').model('bpclight','select kuni, LibUnic, LibUni from unites where archive = 0 and kets = 1',cb);
 //		Unite.using('db').model('bpclight','select nom,prenom,matri from agents',cb);
 		//		Unite.using('db').model('bpclight',q.sql('get_Unite'),cb); 
 	}
