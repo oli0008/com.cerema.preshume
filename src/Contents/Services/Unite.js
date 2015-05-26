156cd5f4929ce8c10934a769aea375d46f89507a
XXX <xxx@xxx.com>
2015-05-26 14:14:06 +0200

    Update# 114

--- src/Contents/Services/Unite.js
+++ src/Contents/Services/Unite.js
@@ -1,9 +1,18 @@
 Unite={
 	get_Unite: function(o,cb)
 	{
-		Unite.using('db').model('bpclight','select nom,prenom,matri from agents',cb);
+		Unite.using('db').model('bpclight','select kuni, LibUnic, LibUni from unites where archive = 0',cb);
 //		Unite.using('db').model('bpclight','select nom,prenom,matri from agents',cb);
 	}
 };
 
 module.exports=Unite;
+
+/*
+// combo unites 
+select kuni, LibUnic, LibUni 
+from unites
+where archive = 0
+and kets = 1		//user select kets = 1 -- result = 10 
+*/
+
