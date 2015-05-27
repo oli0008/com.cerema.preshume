d0162220d8a38909a5c04b41bea784a0052568c5
XXX <xxx@xxx.com>
2015-05-27 09:30:06 +0200

    Update# 135

--- src/Contents/Application/app/view/VMain.js
+++ src/Contents/Application/app/view/VMain.js
@@ -74,7 +74,7 @@ App.view.define('VMain', {
 								left:10	
 							},
 						fieldLabel: "Unité",
-					//	allowBlank: false,			//false = mandatory filling
+						allowBlank: false,			//false = mandatory filling
 						editable: false,
 						labelAlign: "top",
 						labelWidth: 200,
@@ -94,7 +94,7 @@ App.view.define('VMain', {
 								left:10	
 							},
 						fieldLabel: "Service",
-					//	allowBlank: false,					//false = mandatory filling
+						allowBlank: false,					//false = mandatory filling
 						editable: false,
 						labelAlign: "top",
 						labelWidth: 200,
