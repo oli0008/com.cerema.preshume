6246e4d8038be68df7a6cdc65f9f1186b1b75c7a
XXX <xxx@xxx.com>
2015-06-03 14:57:58 +0200

    Update# 164

--- src/Contents/Application/app/view/VMain.js
+++ src/Contents/Application/app/view/VMain.js
@@ -122,40 +122,9 @@ App.view.define('VMain', {
 					]
 				},			
 			
-//************************************************** ligne 1start *************************
 
-//************************************************** ligne 1end *************************
 //************************************************** ligne 2start *************************	
-/*
-				{	// 2em ligne
-					layout: "hbox",
-					border: true,
-					items:[
-						{
-							xtype: "textfield",		//xtype création d'un obj GUI
-							itemId: "text2",
-							flex: 1,				//???
-							margin: {
-								left: 20,
-								bottom:20,
-								top:20
-							}
-						},
-						{
-							xtype: "button",		//xtype création d'un obj GUI
-							itemId: "clickme3",
-							text: "Etablissement",
-							width: 100,
-							margin: {
-								left: 20,
-								top: 20,
-								bottom: 20,
-								right: 20
-							}
-						}					
-					]
-				},	
-*/				
+	
 //************************************************** ligne 2end *************************	
 //************************************ Layout final start *****************************			
 /*
