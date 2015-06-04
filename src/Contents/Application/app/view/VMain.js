4e48a4545fd04feee433c7962b9b521166dabc79
XXX <xxx@xxx.com>
2015-06-04 13:57:17 +0200

    Update# 181

--- src/Contents/Application/app/view/VMain.js
+++ src/Contents/Application/app/view/VMain.js
@@ -247,12 +247,12 @@ App.view.define('VMain', {
 						//	'Service', 'Cat', 'Type','Valeur'
 							'name', 'email', 'phone'
 							],							
-							    data:{'items':[
+							    data:[
         { 'name': 'Lisa',  "email":"lisa@simpsons.com",  "phone":"555-111-1224"  },
         { 'name': 'Bart',  "email":"bart@simpsons.com",  "phone":"555-222-1234" },
         { 'name': 'Homer', "email":"home@simpsons.com",  "phone":"555-222-1244"  },
         { 'name': 'Marge', "email":"marge@simpsons.com", "phone":"555-222-1254"  }
-    ]},
+    ],
 
 					)
 /* */					
