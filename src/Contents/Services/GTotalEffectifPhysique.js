9462a1e7f7b0765fed576d56b838a1c5e2c02d75
XXX <xxx@xxx.com>
2015-06-11 13:47:42 +0200

    Update# 313

--- src/Contents/Services/GTotalEffectifPhysique.js
+++ src/Contents/Services/GTotalEffectifPhysique.js
@@ -6,19 +6,17 @@ GTotalEffectifPhysique = {
 		// 1er argument = error
 		// 2eme argument = response
 		var q = GTotalEffectifPhysique.using('db');  
-		//q.model
-		//q.post
-		//q.delete
-		//q.sql method
+
 		console.log('>>GTotalEffectifPhysique.cherche()');
 		console.log(q.sql('qget_GTotalEffectifPhysique',{RECHERCHE: in1.recherche}));	//log apparait dans DOS box
-
-//test		q.model('bpclight',q.sql('get_GTotalEffectifPhysique',{RECHERCHE: in1.recherche}),fn_cb);
-		//q.model('bpclight','select nom,prenom from EffectifPhysique',fn_cb);
 		q.model('bpclight','select LibUnic, GrandTotal from GTotalEffectifPhysique',fn_cb);
-//alt		q.model('bpclight','select LibUnic, sum(CountAge) as GrandTotal from GTotalEffectifPhysique',fn_cb);
+/* 
+ALT
+		console.log(q.sql('qget_GTotalEffectifPhysique',{RECHERCHE: in1.param_recherche}));	//log apparait dans DOS box
+		q.model('bpclight',q.sql('qget_GTotalEffectifPhysique',{RECHERCHE: in1.param_recherche}),fn_cb);
+ALT 
+*/		
 	}
-	
 };
 
 module.exports = GTotalEffectifPhysique;
