7ccd0a2a0046fe391df10a7f9e48728a65709fec
XXX <xxx@xxx.com>
2015-06-11 12:38:03 +0200

    Update# 306

--- src/Contents/Services/Service.js
+++ src/Contents/Services/Service.js
@@ -1,7 +1,22 @@
 // class Service
 // class omneedia db = classe moteur d'abstraction de base de données
 Service = {
-	
+	get_service: function(in1,fn_cb) {
+		// 1er argument = error
+		// 2eme argument = response
+		
+  		var q = Service.using('db');
+
+		console.log('>>Service.get_service()');
+		console.log(q.sql( 'qget_service' + {RECHERCHE: in1.id_Service} ) );		//log apparait dans DOS box
+		q.model('bpclight',q.sql('qget_service',{RECHERCHE: in1.id_Service}),fn_cb);
+	}
+};
+
+module.exports = Service;
+
+
+//////////////
 /* **	
 
 	get_service: function(o,cb)
@@ -28,18 +43,3 @@ from subdis
 where archive = 0 
 and kuni = 13		// user select kuni = 13 -- result = 6 
 */
-
-	get_service: function(in1,fn_cb) {
-		// 1er argument = error
-		// 2eme argument = response
-		
-  		var q = Service.using('db');
-
-		console.log('>>Service.get_service()');
-		console.log(q.sql( 'qget_service' + {RECHERCHE: in1.id_Service} ) );		//log apparait dans DOS box
-		q.model('bpclight',q.sql('qget_service',{RECHERCHE: in1.id_Service}),fn_cb);
-	}
-};
-
-module.exports = Service;
-
--- src/Contents/Services/sql/qget_service.sql
+++ src/Contents/Services/sql/qget_service.sql
@@ -1,21 +1,14 @@
 /* combo services */
 
-/*
-select kuni, LibUnic, LibUni 
-from unites
-where archive = 0
-and kets = {RECHERCHE}
-*/
-/*
-and kets = 1		
-*/
-/* user select kets = 1 -- result = 10 */
-
-
 select kuni, LibSubc, LibSub 
 from subdis 
 where archive = 0 
 and kuni =  {RECHERCHE}
-/*
-and kuni = "' + o.id_Service + '" ',cb);
+
+
+/* combo services 
+select kuni, LibSubc, LibSub  
+from subdis 
+where archive = 0 
+and kuni = 13		// user select kuni = 13 -- result = 6 
 */
