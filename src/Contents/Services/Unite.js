55a882661b680c3799d8a6acd31363c89d3ae0e6
XXX <xxx@xxx.com>
2015-06-11 12:21:24 +0200

    Update# 302

--- src/Contents/Services/Unite.js
+++ src/Contents/Services/Unite.js
@@ -14,24 +14,3 @@ Unite = {
 };
 
 module.exports = Unite;
-
-/*
-// combo unites 
-select kuni, LibUnic, LibUni 
-from unites
-where archive = 0
-and kets = 1		//user select kets = 1 -- result = 10 
-*/
-
-//temp	
-// ** test	*/
-/*
-//	{
-		console.log('select kuni, LibUnic, LibUni from unites where archive = 0 and kets = "' + o.id_Etablis + '" ');
-		console.log('V2 -- select kuni, LibUnic, LibUni from unites where archive = 0 and kets = "' + o.id_Etablis + '" µµOLI<o.id_Etablis>= ' + o.id_Etablis);
-		Unite.using('db').model('bpclight','select kuni, LibUnic, LibUni from unites where archive = 0 and kets = "' + o.id_Etablis + '" ',cb);
-//		Unite.using('db').model('bpclight','select kuni, LibUnic, LibUni from unites where archive = 0 and kets = 1',cb);
-//		Unite.using('db').model('bpclight','select nom,prenom,matri from agents',cb);
-		//		Unite.using('db').model('bpclight',q.sql('get_Unite'),cb); 
-	}
-*/
