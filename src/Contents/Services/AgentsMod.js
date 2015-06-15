01da2f61350e8ebbf2bdab0e5d6a3607d7b089d1
XXX <xxx@xxx.com>
2015-06-15 18:58:59 +0200

    Update# 525

--- src/Contents/Services/AgentsMod.js
+++ src/Contents/Services/AgentsMod.js
@@ -90,8 +90,10 @@ AgentsMod = {
 //		console.log(q.sql( 'qget_service' + {RECHERCHE: in1.id_Service} ) );		//log apparait dans DOS box
 //		q.model('bpclight',q.sql('XXX_qget_service'),fn_cb);
 		q.model('bpclight', 'select id_domaine, nom_domaine from domaine order by id_domaine asc', fn_cb);
-	},		
+	},
 	
+/* TODO link combos */	
+
 	get_thematique: function(in1,fn_cb) {
 		
   		var q = AgentsMod.using('db');
