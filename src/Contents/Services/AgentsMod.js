8dace0dbf9bb41fc5d77b4e63f2393f11237a0cd
XXX <xxx@xxx.com>
2015-06-15 18:53:43 +0200

    Update# 522

--- src/Contents/Services/AgentsMod.js
+++ src/Contents/Services/AgentsMod.js
@@ -91,8 +91,9 @@ AgentsMod = {
 		console.log('>>AgentsMod.get_thematique()');
 		console.log(q.sql( 'qget_service' + {RECHERCHE: in1.id_Service} ) );		//log apparait dans DOS box
 //		q.model('bpclight',q.sql('XXX_qget_service',{RECHERCHE: in1.id_Service}),fn_cb);
-		q.model('bpclight',q.sql('qget_thematique'),fn_cb);
+//		q.model('bpclight',q.sql('qget_thematique'),fn_cb);
 //		AgentsMod.using('db').model('bpclight', 'select id_thematique, nom_thematique from thematiques order by id_thematique asc', fn_cb);
+		AgentsMod.using('db').model('bpclight', 'select id_thematique, nom_thematique from thematiques where id_domaine = 4 order by id_thematique asc', fn_cb);
 	},	
 
 	get_domaine_intervention: function(in1,fn_cb) {
