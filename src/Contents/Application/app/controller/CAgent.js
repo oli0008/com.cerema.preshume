61fe98e3b4dc56de94fafbe432e3fe8826c6f958
XXX <xxx@xxx.com>
2015-06-16 18:20:11 +0200

    Update# 608

--- src/Contents/Application/app/controller/CAgent.js
+++ src/Contents/Application/app/controller/CAgent.js
@@ -96,8 +96,8 @@ App.controller.define('CAgent', {
 		
 		//met à jour gridAgents pour cette valeur d'établissement
 //		App.get('grid#gridAgents').getStore().getProxy().extraParams.id_Etablis = KetsTemp;
-//		App.get('grid#gridAgents').getStore().getProxy().extraParams.id_Etablis = KetsTemp;
-//		App.get('grid#gridAgents').getStore().load();
+		App.get('grid#gridAgents').getStore().getProxy().extraParams.id_Etablis = KetsTemp;
+		App.get('grid#gridAgents').getStore().load();
 	},
 	
 	//Sélectionner une unité affiche le service correspondant (cela active le store du service)
