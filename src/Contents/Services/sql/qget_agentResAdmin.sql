2949f5bce57e7070eeebcc54aa02a0695fb68909
XXX <xxx@xxx.com>
2015-06-17 13:04:57 +0200

    Update# 655

--- src/Contents/Services/sql/qget_agentResAdmin.sql
+++ src/Contents/Services/sql/qget_agentResAdmin.sql
@@ -1,4 +1,7 @@
-select r.id_residence, concat(rue_residence, "'" - "'", ville_residence) as rue_ville_residence 
+select r.id_residence, concat(rue_residence, ' - ', ville_residence) as rue_ville_residence 
 from residence_admin r
 	join agents a
 		on r.id_residence = a.id_residence
+where a.Kage = {ID_AGENT}
+
+ 
