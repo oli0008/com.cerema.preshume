dc4cc39f4caf2bfba8c81f9aaf23b8d87328c843
XXX <xxx@xxx.com>
2015-06-17 11:08:52 +0200

    Update# 647

--- src/Contents/Application/app/controller/CAgent.js
+++ src/Contents/Application/app/controller/CAgent.js
@@ -141,11 +141,10 @@ App.controller.define('CAgent', {
 		//Récupére l'identifiant de l'agent sélectionné
 //		var agent_id = App.get('agent grid#gridAgents').getValue(record.data.Nom);
 		var agent_id = 333;
-//		console.log(agent_id);
+		alert('agent_id = ' + agent_id );
+		console.log(agent_id);
 	//	alert('vous avez fait des erreurs:\n '+errors.join('\n'));
-		alert('agent_id = ' + agent_id.join ); 
-			alert('agent_id = ' + agent_id ); 
-//				alert(agent_id );
+ 
 /* 		if (id) {
 			var obj = {
 				matri 	: id,
