3d138072ae2dedb71d5fcb3e619c3ff40f5d940d
XXX <xxx@xxx.com>
2015-06-17 10:59:02 +0200

    Update# 641

--- src/Contents/Application/app/controller/CAgent.js
+++ src/Contents/Application/app/controller/CAgent.js
@@ -140,9 +140,10 @@ App.controller.define('CAgent', {
 		alert('selectAgentsDetails XXX');
 		//Récupére l'identifiant de l'agent sélectionné
 //		var agent_id = App.get('agent grid#gridAgents').getValue();
+		var agent_id = App.get('agent grid#gridAgents').getValue(record.data.Kage);
 //		console.log(agent_id);
 	//	alert('vous avez fait des erreurs:\n '+errors.join('\n'));
-		alert('agent_id = ');// + agent_id.join );
+		alert('agent_id = ' + agent_id.join );
 /* 		if (id) {
 			var obj = {
 				matri 	: id,
