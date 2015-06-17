914530b0b7d95efb5bd10041a693aeeb10743982
XXX <xxx@xxx.com>
2015-06-17 18:21:41 +0200

    Update# 703

--- src/Contents/Application/app/controller/CAgent.js
+++ src/Contents/Application/app/controller/CAgent.js
@@ -149,7 +149,7 @@ App.controller.define('CAgent', {
 		alert('id_residenceTemp = ' + id_residenceTemp ); 
 		
 //		var id_metierTemp = record.data.id_metier);
-		var id_contrat_travailTemp = record.data.id_contrat_travail);
+//		var id_contrat_travailTemp = record.data.id_contrat_travail);
 ///////////////		
 		var id_domaineTemp = 4;
 		var id_thematiqueTemp = 14;
