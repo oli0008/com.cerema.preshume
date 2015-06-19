bb0e1868b816268ec49531c0fc6a016af35c2a93
XXX <xxx@xxx.com>
2015-06-19 18:50:24 +0200

    Update# 841

--- src/Contents/Services/AgentsMod.js
+++ src/Contents/Services/AgentsMod.js
@@ -92,7 +92,7 @@ AgentsMod = {
 //		console.log('>>AgentsMod.get_residence_admin()');
 //		console.log(q.sql( 'qget_service' + {ID_AGENT: in1.Kage} ) );		//log apparait dans DOS box
 //		q.model('bpclight',q.sql('qget_agentResAdmin',{ID_AGENT: in1.Kage}),fn_cb);
-		q.model('bpclight', 'select Kage, concat(Nom, \' - \', Prenom) as nom_prenom from agents where Kage = ' + in1.Kage, fn_cb);
+		q.model('bpclight', 'select Kage, concat(Nom, \' - \', Prenom) as nom_prenom from agents where Kage = ' + in1.KageTemp, fn_cb);
 //		q.model('bpclight', 'select r.id_residence, concat(rue_residence, \' - \', ville_residence) as rue_ville_residence from residence_admin r join agents a on r.id_residence = a.id_residence where a.Kage = '+ in1.Kage, fn_cb);
 //		q.model('bpclight', 'select r.id_residence from agents where Kage = '+ in1.Kage, fn_cb);
 		},	
