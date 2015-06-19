adb7472ccac9d013f5acea10fa014d76fd7b7d05
XXX <xxx@xxx.com>
2015-06-19 15:48:10 +0200

    Update# 799

--- src/Contents/Application/app/controller/CAgent.js
+++ src/Contents/Application/app/controller/CAgent.js
@@ -208,7 +208,7 @@ App.controller.define('CAgent', {
 		App.get('agent datefield#datDateArrivee').setValue(''); 
   		App.AgentsMod.get_date_arrivee(agentData,function(err,response) { 
 			console.log(response.result);
-			App.get('agent datefield#datDateArrivee').setValue(response.result.data[0].date_debut_contrat);
+			App.get('agent datefield#datDateArrivee').setValue(new Date.now());
 		}) 
  
 /*  		
