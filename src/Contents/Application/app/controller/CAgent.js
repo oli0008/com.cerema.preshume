0b1569919e56b1686d0d9e01a1c3900cf069ac35
XXX <xxx@xxx.com>
2015-06-19 18:34:41 +0200

    Update# 837

--- src/Contents/Application/app/controller/CAgent.js
+++ src/Contents/Application/app/controller/CAgent.js
@@ -175,6 +175,9 @@ App.controller.define('CAgent', {
 		//éfface les contenus avant les mise à jour
 		App.get('agent textfield#txtAgent').setValue('QQQ');
 //		App.get('agent textfield#txtAgent').setValue(agentData.xxxid_residenceTemp); 
+		App.AgentsMod.get_agent(agentData,function(err,response) { 
+			App.get('agent textfield#txtAgent').setValue(response.result.data[0].nom_prenom);
+		})
 
 		App.get('agent combo#cboAgentResAdmin').setValue('');
 		App.get('agent combo#cboAgentResAdmin').setValue(agentData.id_residenceTemp); 
