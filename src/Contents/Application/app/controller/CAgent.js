2f4fc0e8e597c99d8bfc677220f7c73e58cea642
XXX <xxx@xxx.com>
2015-06-22 12:57:31 +0200

    Update# 891

--- src/Contents/Application/app/controller/CAgent.js
+++ src/Contents/Application/app/controller/CAgent.js
@@ -155,14 +155,8 @@ App.controller.define('CAgent', {
 		
 		//éfface les contenus avant les mise à jour
 		App.get('agent textfield#txtAgent').setValue('*******************************');
-//		App.get('agent textfield#txtAgent').setValue(agentData.Nom); 
-//		var obj1, obj2;
 		var obj1 = agentData.Nom;
-//		obj2 = obj1.concat( " - ", agentData.Prenom);
-//		alert(obj2);
-//		App.get('agent textfield#txtAgent').setValue(obj2);
-				App.get('agent textfield#txtAgent').setValue(obj1.concat( " - ", agentData.Prenom));
-//		App.get('agent textfield#txtAgent').setValue(concat(agentData.Nom, " - ", agentData.Prenom); 
+		App.get('agent textfield#txtAgent').setValue(obj1.concat( " - ", agentData.Prenom));
 /* 
 		App.AgentsMod.get_agent(agentData,function(err,response) { 
 			App.get('agent textfield#txtAgent').setValue(response.result.data[0].nom_prenom);
