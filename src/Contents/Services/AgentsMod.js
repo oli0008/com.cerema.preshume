027fbc8357305f022ffbfef20ac1f499e20e3b61
XXX <xxx@xxx.com>
2015-06-22 17:11:20 +0200

    Update# 948

--- src/Contents/Services/AgentsMod.js
+++ src/Contents/Services/AgentsMod.js
@@ -55,7 +55,7 @@ AgentsMod = {
 	get_agentsByEtablis: function(in1, fn_cb) {	
 	  	var q = AgentsMod.using('db');
 		console.log('>>AgentsMod.get_agentsByEtablis()HHH');
-//	XXX	alert('AgentsMod.get_agentsByEtablis');
+	alert('AgentsMod.get_agentsByEtablis');
 	//	alert('AgentsMod.get_agentsByEtablis() in1.id_Etablis = ' + in1.id_Etablis);
 //		console.log(q.sql( 'qget_agentsByEtablis' + {P_ETABLIS: in1.id_Etablis} ) );		//log apparait dans DOS box
 		// Temp fix: have set Kets = 1
