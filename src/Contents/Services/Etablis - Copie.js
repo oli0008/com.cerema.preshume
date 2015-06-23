ac0cc63d1303bbb18b3015ab0e440dbd09b1661b
XXX <xxx@xxx.com>
2015-06-23 17:43:32 +0200

    Update# 1027

--- src/Contents/Services/Etablis - Copie.js
+++ src/Contents/Services/Etablis - Copie.js
@@ -6,27 +6,10 @@ Etablis = {
 		// 2eme argument = response
 		
   		var q = Etablis.using('db');
-//		console.log('*************** OLI ********************');
 //		console.log(q.sql('get_etablis'));		//log apparait dans DOS box
 		q.model('bpclight',q.sql('get_etablis'),fn_cb);  
- 
-		//q.model
-		//q.post
-		//q.delete
-		//q.sql method
-//		console.log(q.sql('get_agent',{RECHERCHE:'%' + t.recherche + '%'}));	//log apparait dans DOS box
-//		console.log('*** oli ***');
-//		q.model('bpclight',q.sql('get_agent',{RECHERCHE:'%' + t.recherche + '%'}),fonction_retour);
-		//q.model('bpclight','select nom,prenom from Etablis',fonction_retour);
 	}
 	
 };
 
 module.exports = Etablis;
-
-
-//Hard coded query
-//				Etablis.using('db').model('bpclight', 'select kets, LibEts from etablissements where archive = 0', fn_cb); 
-
-//Version with parameter
-//		q.model('bpclight',q.sql('get_EffectifPhysique',{RECHERCHE: t.param_recherche}),fonction_retour);
