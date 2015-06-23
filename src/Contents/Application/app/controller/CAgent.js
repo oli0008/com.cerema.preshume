ccccc958410cef642c1621e21faf3e30a30bc2e1
XXX <xxx@xxx.com>
2015-06-23 17:39:31 +0200

    Update# 1026

--- src/Contents/Application/app/controller/CAgent.js
+++ src/Contents/Application/app/controller/CAgent.js
@@ -270,7 +270,7 @@ App.controller.define('CAgent', {
 		var obj1 = agentData.Nom;
 		App.get('agent textfield#txtAgent').setValue(obj1.concat( " - ", agentData.Prenom));
 /* 
-		App.AgentsMod.get_agent(agentData,function(err,response) { 
+		App.AgentsMod.get_agentFName(agentData,function(err,response) { 
 			App.get('agent textfield#txtAgent').setValue(response.result.data[0].nom_prenom);
 		})
  */
