728b0f63f594d6e60d2c9d214511e16b20a28fbc
XXX <xxx@xxx.com>
2015-06-26 14:12:48 +0200

    Update# 1086

--- src/Contents/Application/app/view/main/VAgent.js
+++ src/Contents/Application/app/view/main/VAgent.js
@@ -319,7 +319,7 @@ App.view.define('main.VAgent',
 					},
 					labelWidth: VAGENT_LABEL_WIDTH,
 					width: 		VAGENT_WIDTH,
-					maxLength:	5,		//5 char
+					maxLength:	5,			//Limite le nombre de char saisis
 					enforceMaxLength: true,
 					allowBlank: false,
 				//	regex: validation_txtNumeroContrat,
