5d44cce716c2e1e011a5ffdb330a4a95acfa24b1
XXX <xxx@xxx.com>
2015-06-26 13:47:44 +0200

    Update# 1074

--- src/Contents/Application/app/controller/CAgent.js
+++ src/Contents/Application/app/controller/CAgent.js
@@ -12,8 +12,8 @@
 //nok  var G_BASE_DONNEES = 'bpclight';	//base de données de l'application
 
 // Dimmentionnement des objets d'interface
-var VAgent_label_width = 	350;	//VAGENT_LABEL_WIDTH
-var VAgent_width = 			350;	//VAGENT_WIDTH
+var VAGENT_LABEL_WIDTH = 	350;	//VAGENT_LABEL_WIDTH
+var VAGENT_WIDTH = 			350;	//VAGENT_WIDTH
 
 // Variables de validation
 var validation_txtNumeroContrat = new RegExp ("[0-9]{5}");	//("#[0-9]{5}#");
