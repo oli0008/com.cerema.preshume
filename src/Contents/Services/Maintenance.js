10401eededd20c2d7ca2dbe90089b1eda3afb749
XXX <xxx@xxx.com>
2015-07-01 17:30:35 +0200

    Update# 1191

--- src/Contents/Services/Maintenance.js
+++ src/Contents/Services/Maintenance.js
@@ -37,9 +37,8 @@ Maintenance = {
 	read: function(in1,fn_cb) {
   		var q = Maintenance.using('db');
 		console.log('SELECT id_residence, rue_residence, code_postal_residence, ville_residence FROM residence_admin');
-//		q.model(BASE_DONNEES, 'SELECT id_residence, rue_residence, code_postal_residence, ville_residence FROM residence_admin',cb);
-		Maintenance.using('db').query('bpclight','SELECT id_residence, rue_residence, code_postal_residence, '
-				+' ville_residence FROM residence_admin',fn_cb);
+		q.model(BASE_DONNEES, 'SELECT id_residence, rue_residence, code_postal_residence, ville_residence FROM residence_admin', fn_cb);
+//		Maintenance.using('db').query('bpclight','SELECT id_residence, rue_residence, code_postal_residence, ville_residence FROM residence_admin',fn_cb);
 	},
 	
 	update: function(in1,fn_cb) {	
