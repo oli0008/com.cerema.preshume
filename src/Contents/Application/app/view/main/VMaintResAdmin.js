cb4595b7a160424a60740f9de6e01f2aebe1023b
XXX <xxx@xxx.com>
2015-07-01 16:45:52 +0200

    Update# 1167

--- src/Contents/Application/app/view/main/VMaintResAdmin.js
+++ src/Contents/Application/app/view/main/VMaintResAdmin.js
@@ -84,8 +84,8 @@ App.view.define('main.VMaintResAdmin', {
 					xtype		: "textfield",
 					itemId		: "txtRueResAdmin",
 //					labelAlign	: "left",
-					labelWidth	: VRESADMIN_LABEL_WIDTH,
-					width		: VRESADMIN_WIDTH,
+//					labelWidth	: VRESADMIN_LABEL_WIDTH,
+//					width		: VRESADMIN_WIDTH,
 					maxLength:	5,			//Limite le nombre de char dans ce champ.
 					enforceMaxLength: true,
 					allowBlank: false,	
