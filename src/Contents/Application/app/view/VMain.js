06ac28b5f6e8458c89037f11ec62b2bf7d4748a5
XXX <xxx@xxx.com>
2015-07-02 11:33:35 +0200

    Update# 1219

--- src/Contents/Application/app/view/VMain.js
+++ src/Contents/Application/app/view/VMain.js
@@ -1,337 +1,240 @@
 /********************************************************
-*
-*	Vue principale de l'application. Elle est gérée par le controlleur principal.
-*
-*	Fonction: Afficher les composants graphiques de la fenêtre ( menu principale & composants de la fenêtre principale).
-*
-***********************************************************/
-
+ *
+ *	Vue principale de l'application. Elle est gérée par le controlleur principal.
+ *
+ *	Fonction: Afficher les composants graphiques de la fenêtre ( menu principale & composants de la fenêtre principale).
+ *
+ ***********************************************************/
 App.view.define('VMain', {
 
     extend: 'Ext.Panel',
-	alias : 'widget.mainform',
-	border: false,
-	
-	layout: "border",
-	
-	items: [
-	{
-//*************************************** NORTH - Menus ****************************
-		region: 'north',
-		height: 25,
-		minHeight: 25,
-		border:false,	//cache la ligne de bordure 
-		baseCls: 'cls-header',
-		xtype: "Menu",		//xtype création d'un obj GUI
-		itemId: "MenuPanel",
-		menu: [
-		{
-			//Menu Effectifs
-			text: "Effectifs",
-//			width: G_VMAIN_MNU_WIDTH,
-			menu: [
-			{
-				//Menu item
-				text: "Présent",
-				itemId: "mnuEffectifPresent",
+    alias: 'widget.mainform',
+    border: false,
 
-			},
-			{
-				//Menu item
-				text: "Future",
-				itemId: "mnuEffectifFuture"
-			}	
-			]
-		},
-		{
-			//Menu ETP
-			text: "ETP",
-//			width: G_VMAIN_MNU_WIDTH,
-			menu: [
-			{
-				//Menu item
-				text: "Présent",
-				itemId: "mnuETPPresent"
-			},
-			{
-				//Menu item
-				text: "Future",
-				itemId: "mnuETPFuture"
-			}	
-			]
-		},
-		{
-			//Menu Agents
-			text: "Agents",
-//			width: G_VMAIN_MNU_WIDTH,
-			menu: [
-			{
-				//Menu item
-				text: "Modification",
-				itemId: "mnuModAgent"
-			}
-			]
-		},
-		{
-			//Menu Maintenance
-			text: "Maintenance",
-//			width: G_VMAIN_MNU_WIDTH,
-			menu: [
-			{
-				//Menu item
-				text: "Résidence administrative",
-				itemId: "mnuResAdmin"
-			},
-			{
-				//Menu item
-				text: "Thématique",
-				itemId: "mnuThematique"
-			},
-			{
-				//Menu item
-				text: "Domaine d'intervention",
-				itemId: "mnuDomaineInterv"
-			},
-			{
-				//Menu item
-				text: "Type de contrat",
-				itemId: "mnuTypeContrat"
-			},
-			{
-				//Menu item
-				text: "Métier",
-				itemId: "mnuMetier"
-			},
-			{
-				//Menu item
-				text: "Catégorie",
-				itemId: "mnuCategorie"
-			},
-			{
-				//Menu item
-				text: "Grade",
-				itemId: "mnuGrade"
-			}					
-			]
-		}
-		]				
-	},
-//********************************************* CENTER *******************************
-	{
-			region: "center",			
-			split:true,
-			items: [
-			{	// 1er ligne
-				layout: "hbox",
-				border: true,	//false,
-				width: "100%",
-				items:[
-				{
-					xtype: "combo",
-					itemId: "cboMainEtablis",
-					margin: {
-							top: 10,
-							left:10	
-							},
-					fieldLabel: "Etablissement",
-				//	allowBlank: false,		//false = mandatory filling
-					editable: false,
-					labelAlign: "top",
-					labelWidth: G_VMAIN_CBO_WIDTH,	
-					width: 		G_VMAIN_CBO_WIDTH,
- 					displayField: "LibEts",
-					valueField: "Kets",									
-					store: App.store.create("App.Etablis.get_etablis", {
-													autoLoad: true
-												}) 		// Creation du store				
-				},
-				//*************************
-				{
-					xtype: "combo",
-					itemId: "cboMainUnite",
-					margin: {
-							top: 10,
-							left:10	
-							},
-					fieldLabel: "Unité",
-				//	allowBlank: true,			//false = red notification
-					editable: false,
-					labelAlign: "top",
-					labelWidth: G_VMAIN_CBO_WIDTH,	
-					width: 		G_VMAIN_CBO_WIDTH,
- 					displayField: "LibUni",		//Alt use: LibUnic
-					valueField: "Kuni",									
-					store: App.store.create("App.Unite.get_unite", {
-													autoLoad: true
-												})  	// Creation du store
-				},
-				{
-					xtype: "combo",
-					itemId: "cboMainService",
-					margin: {
-							top: 10,
-							left:10	
-							},
-					fieldLabel: "Service",
-				//	allowBlank: false,					//false = mandatory filling
-					editable: false,
-					labelAlign: "top",
-					labelWidth: G_VMAIN_CBO_WIDTH,	
-					width: 		G_VMAIN_CBO_WIDTH,
- 					displayField: "LibSubc",
-					valueField: "Ksub",			
-					store: App.store.create("App.Service.get_service", {
-													autoLoad: true
-												})  // Creation du store	
-				},
-				//*************************
-				{
-					xtype: "datefield",
-					fieldLabel: "Date",
-					labelAlign: "top",
-//renderer DOES NOT WORK!!
-					renderer: Ext.util.Format.dateRenderer('dd/mm/YYYY'),
-//					renderer: Ext.util.Format.dateRenderer("Y-m-d"), //('m/d/Y'),
-//					renderer: Ext.util.Format.dateRenderer('d/m/Y'),					
-					itemId: "datMainDate",
-					margin: {
-							top: 10,
-							left: 10,
-							bottom: 10
-					},
-					labelWidth: G_VMAIN_CBO_WIDTH,
-					width: 		G_VMAIN_CBO_WIDTH,
-					allowBlank: false,
-				},	 				
-				//*************************
-				{
-					xtype: "button",		//xtype création d'un obj GUI
-					itemId: "btnMainOk",	//xxxx"clickme",
-					text: "Ok", 
-					width: 50,
-					margin: {
-							left: 20,
-							top: 28,
-							bottom: 20,
-							right: 20
-							}
-				}							
-				]
-			},			
+    layout: "border",
 
-//************************************ Layout final start *****************************			
-/*
-				{	//grid1_start
-					layout: "hbox",
-					border: true,
-					items:[
-					{
-							xtype: "grid",			//xtype création d'un obj GUI
-							itemId: "gridTotalsPresent",
-							columns: [
-							{
-							text: "A+",
-							dataIndex: "LibCgr"
-							},
-							{
-							text: "A",
-							dataIndex: "agentTotal"
-							},
-							{
-								text: "B",
-								dataIndex: "agentTotal"
-							},
-							{
-								text: "C",
-								dataIndex: "agentTotal"
-							},
-							{
-								text: "Exploitation",
-								dataIndex: "LibUnic"
-							}
-						],
-						flex: 1,
-					//	width: "100%",
-						width: "50%",
-				//		store: App.store.create("App.Agents.cherche")		//???
-						store: App.store.create("App.EffectifPhysique.cherche")	
-				},	//grid1_end
-				{	//grid2_start
-					xtype: "grid",			//xtype création d'un obj GUI
-					itemId: "gridGrandTotalpresent",
-					columns: [
-					{
-						text: "Total",
-						dataIndex: "GrandTotal"	//sum(CountAge) as GrandTotal
-					}
-					],
-					flex: 1,
-				//	width: "100%",
-					width: "50%",
-			//		store: App.store.create("App.Agents.cherche")		//???
-			//		store: App.store.create("App.EffectifPhysique.cherche")	
-					store: App.store.create("App.EffectifPhysique.cherche")	
-				}	//grid2_end
-				]
-				},
-*/	
-//************************************ Layout final end *****************************					
+    items: [{
+            //*************************************** NORTH - Menus ****************************
+            region: 'north',
+            height: 25,
+            minHeight: 25,
+            border: false, //cache la ligne de bordure 
+            baseCls: 'cls-header',
+            xtype: "Menu", //xtype création d'un obj GUI
+            itemId: "MenuPanel",
+            menu: [{
+                //Menu Effectifs
+                text: "Effectifs",
+                //			width: G_VMAIN_MNU_WIDTH,
+                menu: [{
+                    //Menu item
+                    text: "Présent",
+                    itemId: "mnuEffectifPresent",
 
-//MMMMMMMMMMMMMMMMMMMMMMMMM
+                }, {
+                    //Menu item
+                    text: "Future",
+                    itemId: "mnuEffectifFuture"
+                }]
+            }, {
+                //Menu ETP
+                text: "ETP",
+                //			width: G_VMAIN_MNU_WIDTH,
+                menu: [{
+                    //Menu item
+                    text: "Présent",
+                    itemId: "mnuETPPresent"
+                }, {
+                    //Menu item
+                    text: "Future",
+                    itemId: "mnuETPFuture"
+                }]
+            }, {
+                //Menu Agents
+                text: "Agents",
+                //			width: G_VMAIN_MNU_WIDTH,
+                menu: [{
+                    //Menu item
+                    text: "Modification",
+                    itemId: "mnuModAgent"
+                }]
+            }, {
+                //Menu Maintenance
+                text: "Maintenance",
+                //			width: G_VMAIN_MNU_WIDTH,
+                menu: [{
+                    //Menu item
+                    text: "Résidence administrative",
+                    itemId: "mnuResAdmin"
+                }, {
+                    //Menu item
+                    text: "Thématique",
+                    itemId: "mnuThematique"
+                }, {
+                    //Menu item
+                    text: "Domaine d'intervention",
+                    itemId: "mnuDomaineInterv"
+                }, {
+                    //Menu item
+                    text: "Type de contrat",
+                    itemId: "mnuTypeContrat"
+                }, {
+                    //Menu item
+                    text: "Métier",
+                    itemId: "mnuMetier"
+                }, {
+                    //Menu item
+                    text: "Catégorie",
+                    itemId: "mnuCategorie"
+                }, {
+                    //Menu item
+                    text: "Grade",
+                    itemId: "mnuGrade"
+                }]
+            }]
+        },
+        //********************************************* CENTER *******************************
+        {
+            region: "center",
+            split: true,
+            items: [{ // 1er ligne
+                    layout: "hbox",
+                    border: true, //false,
+                    width: "100%",
+                    items: [{
+                            xtype: "combo",
+                            itemId: "cboMainEtablis",
+                            margin: {
+                                top: 10,
+                                left: 10
+                            },
+                            fieldLabel: "Etablissement",
+                            //	allowBlank: false,		//false = mandatory filling
+                            editable: false,
+                            labelAlign: "top",
+                            labelWidth: G_VMAIN_CBO_WIDTH,
+                            width: G_VMAIN_CBO_WIDTH,
+                            displayField: "LibEts",
+                            valueField: "Kets",
+                            store: App.store.create("App.Etablis.get_etablis", {
+                                    autoLoad: true
+                                }) // Creation du store				
+                        },
+                        //*************************
+                        {
+                            xtype: "combo",
+                            itemId: "cboMainUnite",
+                            margin: {
+                                top: 10,
+                                left: 10
+                            },
+                            fieldLabel: "Unité",
+                            //	allowBlank: true,			//false = red notification
+                            editable: false,
+                            labelAlign: "top",
+                            labelWidth: G_VMAIN_CBO_WIDTH,
+                            width: G_VMAIN_CBO_WIDTH,
+                            displayField: "LibUni", //Alt use: LibUnic
+                            valueField: "Kuni",
+                            store: App.store.create("App.Unite.get_unite", {
+                                    autoLoad: true
+                                }) // Creation du store
+                        }, {
+                            xtype: "combo",
+                            itemId: "cboMainService",
+                            margin: {
+                                top: 10,
+                                left: 10
+                            },
+                            fieldLabel: "Service",
+                            //	allowBlank: false,					//false = mandatory filling
+                            editable: false,
+                            labelAlign: "top",
+                            labelWidth: G_VMAIN_CBO_WIDTH,
+                            width: G_VMAIN_CBO_WIDTH,
+                            displayField: "LibSubc",
+                            valueField: "Ksub",
+                            store: App.store.create("App.Service.get_service", {
+                                    autoLoad: true
+                                }) // Creation du store	
+                        },
+                        //*************************
+                        {
+                            xtype: "datefield",
+                            fieldLabel: "Date",
+                            labelAlign: "top",
+                            //renderer DOES NOT WORK!!
+                            renderer: Ext.util.Format.dateRenderer('dd/mm/YYYY'),
+                            //					renderer: Ext.util.Format.dateRenderer("Y-m-d"), //('m/d/Y'),
+                            //					renderer: Ext.util.Format.dateRenderer('d/m/Y'),					
+                            itemId: "datMainDate",
+                            margin: {
+                                top: 10,
+                                left: 10,
+                                bottom: 10
+                            },
+                            labelWidth: G_VMAIN_CBO_WIDTH,
+                            width: G_VMAIN_CBO_WIDTH,
+                            allowBlank: false,
+                        },
+                        //*************************
+                        {
+                            xtype: "button", //xtype création d'un obj GUI
+                            itemId: "btnMainOk", //xxxx"clickme",
+                            text: "Ok",
+                            width: 50,
+                            margin: {
+                                left: 20,
+                                top: 28,
+                                bottom: 20,
+                                right: 20
+                            }
+                        }
+                    ]
+                },
 
-//MMMMMMMMMMMMMMMMMMMMMMMMM
-				
-// *********** TEMP FIX START *******************************************	
-				{	// Un panel avec un layout accordion
-					xtype: 'panel',
-					margin:20,
-					title: 'Accordion Layout',
-					width: 300,
-					height: 300,
-					defaults: {
-						// applied to each contained panel
-						/*bodyStyle: 'padding:15px'*/
-					},
-					layout: {
-						// layout-specific configs go here
-						type: 'accordion'
-					},
-					items: [
-					{
-						title: "panel 1",
-						html: "panel1"
-					},
-					{
-						title: "panel 2",
-						html: "panel2"
-					},
-					{
-						title: "panel 3",
-						items: [
-						{							
-							xtype: "grid",
-							itemId: "gridTotalsPresent",
-							margin: 0,
-							columns: [
-							{
-								text: "Unité",
-								dataIndex: "LibUnic"
-								},
-								{
-								text: "Catégorie",
-								dataIndex: "LibCgr"
-								},
-								{
-								text: "Total agent",
-								dataIndex: "agentTotal"
-								}
-							],
-							store: App.store.create("App.EffectifPhysique.cherche")	
-						}
-						]
-					}
-					]
-				}
-			]
-		}
-]});
+
+                { // Un panel avec un layout accordion
+                    xtype: 'panel',
+                    margin: 20,
+                    title: 'Accordion Layout',
+                    width: 300,
+                    height: 300,
+                    defaults: {
+                        // applied to each contained panel
+                        /*bodyStyle: 'padding:15px'*/
+                    },
+                    layout: {
+                        // layout-specific configs go here
+                        type: 'accordion'
+                    },
+                    items: [{
+                        title: "panel 1",
+                        html: "panel1"
+                    }, {
+                        title: "panel 2",
+                        html: "panel2"
+                    }, {
+                        title: "panel 3",
+                        items: [{
+                            xtype: "grid",
+                            itemId: "gridTotalsPresent",
+                            margin: 0,
+                            columns: [{
+                                text: "Unité",
+                                dataIndex: "LibUnic"
+                            }, {
+                                text: "Catégorie",
+                                dataIndex: "LibCgr"
+                            }, {
+                                text: "Total agent",
+                                dataIndex: "agentTotal"
+                            }],
+                            store: App.store.create("App.EffectifPhysique.cherche")
+                        }]
+                    }]
+                }
+            ]
+        }
+    ]
+});
