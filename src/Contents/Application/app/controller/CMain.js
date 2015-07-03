bb81f525316d29efafcb4e2559567b275bb5d8e9
XXX <xxx@xxx.com>
2015-07-03 12:27:06 +0200

    Update# 1347

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -44,7 +44,7 @@ App.controller.define('CMain', {
                     click: "calculerEffectifPresent"
                 },
 				"mainform": {
-					render: "onShow"
+					render: "OnShow"
 				}
             });
 
