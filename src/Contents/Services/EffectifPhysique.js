827c626e331f3fe16be4b444956c168369dbf3e7
XXX <xxx@xxx.com>
2015-07-03 17:46:34 +0200

    Update# 1387

--- src/Contents/Services/EffectifPhysique.js
+++ src/Contents/Services/EffectifPhysique.js
@@ -27,7 +27,18 @@ EffectifPhysique = {
 	
 	
 	//Récupére les effectif présents du mois 	 
-	get_EffPresent: function(in1,fn_cb) {	
+	get_effPresent: function(in1,fn_cb) {	
+  		var q = AgentsMod.using('db');	
+		console.log('select Kets, LibEts from etablissements where archive = 0 and Kets = 1 ');		//log apparait dans DOS box
+	//	q.model(BASE_DONNEES, 'select Kets, LibEts from etablissements where archive = 0 and Kets = 1', fn_cb);
+		q.model(BASE_DONNEES, 'select  lib_departement_ep, lib_categorie_ep, count_agent,  Kets,  date_ep '
+			+ 'from effectif_physique where date_ep >= \'2015-07-01\' and date_ep <= \'2015-07-31\' ', fn_cb);
+//		q.model(BASE_DONNEES,q.sql('qget_etablis'),fn_cb);  
+	},
+	
+	
+	//Récupére les effectif présents du mois 	 
+	get_gdTotalEffPresent: function(in1,fn_cb) {	
   		var q = AgentsMod.using('db');	
 		console.log('select Kets, LibEts from etablissements where archive = 0 and Kets = 1 ');		//log apparait dans DOS box
 	//	q.model(BASE_DONNEES, 'select Kets, LibEts from etablissements where archive = 0 and Kets = 1', fn_cb);
@@ -35,6 +46,7 @@ EffectifPhysique = {
 			+ 'from effectif_physique where date_ep >= \'2015-07-01\' and date_ep <= \'2015-07-31\' ', fn_cb);
 //		q.model(BASE_DONNEES,q.sql('qget_etablis'),fn_cb);  
 	}
+	
 };
 
 module.exports = EffectifPhysique;
