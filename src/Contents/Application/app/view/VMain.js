4670a9e0618e0d09bd60740e5657ff4ec3eb552f
XXX <xxx@xxx.com>
2015-07-07 15:13:00 +0200

    Update# 1429

--- src/Contents/Application/app/view/VMain.js
+++ src/Contents/Application/app/view/VMain.js
@@ -216,9 +216,6 @@ App.view.define('VMain', {
 								ftype: 'groupingsummary'
 								
 							} 
-								
-								
-						//	{ftype:'grouping'}
 							],
 							store: App.store.create("App.EffectifPhysique.get_effPresent",
 							{
@@ -272,15 +269,12 @@ App.view.define('VMain', {
 							columns: [
 							{
 								text: "Département",
-							//	dataIndex: "LibUnic",
 								dataIndex: "lib_departement_ep",
 					//			flex: 1
 							},
 							{
 								text: "Grand Total",
-							//	dataIndex: "LibCgr"
-								dataIndex: "GrandTotal"		//sum(count_agent) as GrandTotal
-					//			dataIndex: "sum(count_agent)"		//sum(count_agent) as GrandTotal								
+								dataIndex: "GrandTotal"		//sum(count_agent) as GrandTotal								
 							}
 							],
 							store: App.store.create("App.EffectifPhysique.get_gdTotalEffPresent")
