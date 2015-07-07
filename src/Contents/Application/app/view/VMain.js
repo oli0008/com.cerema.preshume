e931b281a8f3f551ca045038b846d5f953d0868d
XXX <xxx@xxx.com>
2015-07-07 17:27:05 +0200

    Update# 1494

--- src/Contents/Application/app/view/VMain.js
+++ src/Contents/Application/app/view/VMain.js
@@ -270,7 +270,7 @@ App.view.define('VMain', {
 					//		shadow: true,
       //      store: store1,
 	  store: App.store.create("App.EffectifPhysique.get_effPresent"),
-	  store: App.store.load(),
+	  load: true,
             axes: [{
                 type: 'Numeric',
                 position: 'left',
