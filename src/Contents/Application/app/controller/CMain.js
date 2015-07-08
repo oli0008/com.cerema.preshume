27ebce48bfdd7c1d0fee41d1e08ba08dc97d0e93
XXX <xxx@xxx.com>
2015-07-08 11:15:32 +0200

    Update# 1555

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -96,8 +96,13 @@ App.controller.define('CMain', {
 			this.AfficherEffectifETP(chosenDate);
 	 
 	 
-if (confirm('Voulez-vous exécuter le code JavaScript de cette page ?')) {
-    alert('Le code a bien été exécuté !'); 
+var floor = parseInt(prompt("Entrez l'étage où l'ascenseur doit se rendre (de -2 à 30) :"));
+if (floor == 0) {
+    alert('Vous vous trouvez déjà au rez-de-chaussée.');
+} else if (-2 <= floor && floor <= 30) {
+    alert("Direction l'étage n°" + floor + ' !');
+} else {
+    alert("L'étage spécifié n'existe pas.");
 } 
  
 	 
