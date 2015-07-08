6cc3845b2eb7ecc728bee173369bc799cc8517bb
XXX <xxx@xxx.com>
2015-07-08 11:17:39 +0200

    Update# 1557

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -99,7 +99,7 @@ App.controller.define('CMain', {
 var floor = parseInt(prompt("Entrez l'étage où l'ascenseur doit se rendre (de -2 à 30) :"));
 if (floor == 0) {
     alert('Vous vous trouvez déjà au rez-de-chaussée.');
-} else if (-2 <= floor && floor <= 30) { 
+} else if (-2 <= floor && floor <= 30) {  
     alert("Direction l'étage n°" + floor + ' !');
 } else {
     alert("L'étage spécifié n'existe pas.");
