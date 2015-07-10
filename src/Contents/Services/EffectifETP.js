1b3ffd7df88d056e2f053dc1cc44c8499a508867
XXX <xxx@xxx.com>
2015-07-10 18:24:57 +0200

    Update# 1771

--- src/Contents/Services/EffectifETP.js
+++ src/Contents/Services/EffectifETP.js
@@ -30,8 +30,25 @@ EffectifETP = {
 	get_effectifETPPresent: function(in1,fn_cb) {	
   		var q = AgentsMod.using('db');	
 console.log('get_effectifETPPresent()');
-		q.model(BASE_DONNEES, 'select  lib_departement_etp, lib_categorie_etp, sum_quot,  Kets,  date_etp '
-			+ 'from effectif_etp where date_etp >= \'2015-07-01\' and date_etp <= \'2015-07-31\' ', fn_cb);
+//oo		q.model(BASE_DONNEES, 'select  lib_departement_etp, lib_categorie_etp, sum_quot,  Kets,  date_etp '
+//oo			+ 'from effectif_etp where date_etp >= \'2015-07-01\' and date_etp <= \'2015-07-31\' ', fn_cb);
+// //////////////////			
+		var sql = "select lib_departement_etp, lib_categorie_etp, sum_quot, Kets, date_etp from effectif_physique where date_etp >= '" 
+				+ in1.sd + "' and date_etp <= '" + in1.ed + "' and ";
+		var params= [];
+		if (in1.Kets!= -1) 
+			params.push("Kets=" + in1.Kets);
+		if (in1.Kuni!= -1) 
+			params.push("Kuni=" + in1.Kuni);
+		if (in1.Ksub!= -1) 
+			params.push("Ksub=" + in1.Ksub);		
+		sql += params.join('and ');
+		console.log(sql);
+		q.model(BASE_DONNEES, sql, fn_cb);
+			
+			
+			
+			
 //		q.model(BASE_DONNEES,q.sql('qget_etablis'),fn_cb);  
 	},
 	
