fec1e2fc25623b44756a4af29fc7add562be0cb9
XXX <xxx@xxx.com>
2015-07-10 17:44:29 +0200

    Update# 1758

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -302,15 +302,16 @@ alert(' select_cboMainEtablis -- NOT USED');
 			} else {	//On lit de la table donc il y a une date de début et de fin)
 				//construction des dates de recherche 
 				var dd = TODAY; 	//new Date();
-			alert('prepareDate: TODAY = ' + TODAY);
+			
 				var y = dd.getFullYear();
 				var m = dd.getMonth();
 				
 			alert('prepareDate: m = ' + m);
 				var maxDay = dd.getDaysInMonth(y,m);
 
-				selData.sd = y + '-' + m + '-' + '01';			
+				selData.sd = y + '-' + (m+1) + '-' + '01';			
 				selData.ed =   y + '-' + m + '-' + maxDay;
+			alert('prepareDate: selData.sd = ' + selData.sd);
 			}
 		},				
 		 
