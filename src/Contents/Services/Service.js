824dce75eea67e28a45d38910171a1e41e15aa75
XXX <xxx@xxx.com>
2015-07-16 11:12:51 +0200

    Update# 1815

--- src/Contents/Services/Service.js
+++ src/Contents/Services/Service.js
@@ -5,9 +5,10 @@ Service = {
 		// 1er argument = error
 		// 2eme argument = response		
   		var q = Service.using('db');
-		console.log('>>Service.get_service()');
+		console.log('>>Service.get_service() --START---');
 		console.log('in1.id_Service= ' + in1.id_Service );
-//		console.log(q.sql( 'qget_service' + {RECHERCHE: in1.id_Service} ) );		//log apparait dans DOS box
+		console.log(sql( 'qget_service' + {RECHERCHE: in1.id_Service} ) );		//log apparait dans DOS box
+		console.log('>>Service.get_service() --END---');
 		q.model('bpclight',q.sql('qget_service',{RECHERCHE: in1.id_Service}),fn_cb);
 	}
 };
