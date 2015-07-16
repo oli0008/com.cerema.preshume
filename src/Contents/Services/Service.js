3b84445f103e0c38a4bcf84d212fddb54e9ecf78
XXX <xxx@xxx.com>
2015-07-16 10:17:54 +0200

    Update# 1799

--- src/Contents/Services/Service.js
+++ src/Contents/Services/Service.js
@@ -6,11 +6,9 @@ Service = {
 		// 2eme argument = response		
   		var q = Service.using('db');
 		console.log('>>Service.get_service()');
-//		console.log('in1.id_Service= ' + in1.id_Service );
-		console.log('in1.id_Service= ' + in1.Kuni2 );
+		console.log('in1.id_Service= ' + in1.id_Service );
 //		console.log(q.sql( 'qget_service' + {RECHERCHE: in1.id_Service} ) );		//log apparait dans DOS box
-//		q.model('bpclight',q.sql('qget_service',{RECHERCHE: in1.id_Service}),fn_cb);
-		q.model('bpclight',q.sql('qget_service',{RECHERCHE: in1.Kuni2}),fn_cb);
+		q.model('bpclight',q.sql('qget_service',{RECHERCHE: in1.id_Service}),fn_cb);
 	}
 };
 
