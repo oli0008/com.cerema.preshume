7b7a993ad221f41d6f237136224846275d36f532
XXX <xxx@xxx.com>
2015-07-16 10:50:28 +0200

    Update# 1806

--- src/Contents/Services/Unite.js
+++ src/Contents/Services/Unite.js
@@ -5,10 +5,10 @@ Unite = {
 		// 1er argument = error
 		// 2eme argument = response	
   		var q = Unite.using('db');
-		console.log('>>Unite.get_unite() --- in1.id_Etablis');
+console.log('>>Unite.get_unite() --- START --- in1.id_Etablis');
 				console.log(in1.id_Etablis);
-//		console.log(q.sql( 'qget_unite' + {RECHERCHE: in1.id_Etablis} ) );		//log apparait dans DOS box
-		console.log( 'qget_unite' + {RECHERCHE: in1.id_Etablis} );		//log apparait dans DOS box
+		console.log(q.sql( 'qget_unite' + {RECHERCHE: in1.id_Etablis} ) );		//log apparait dans DOS box
+console.log('>>Unite.get_unite()  --- END --- in1.id_Etablis');
 		q.model('bpclight',q.sql('qget_unite',{RECHERCHE: in1.id_Etablis}),fn_cb);
 	}
 };
