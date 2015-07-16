4ec4927e44c9e74e42e502bde5081999f4e9d221
XXX <xxx@xxx.com>
2015-07-16 11:03:38 +0200

    Update# 1810

--- src/Contents/Services/EffectifPhysique.js
+++ src/Contents/Services/EffectifPhysique.js
@@ -17,8 +17,6 @@ EffectifPhysique = {
 	//Calcule les effectifs Physique présents
 //	 cherche: function(in1,fn_cb) {
 	calculate_EffPresent: function(in1,fn_cb) {
-		// 1er argument = error
-		// 2eme argument = response		
 		var q = EffectifPhysique.using('db');  
 //		console.log('>>EffectifPhysique.cherche()');
 //		console.log(q.sql('qget_EffectifPhysique',{RECHERCHE: in1.param_recherche}));	//log apparait dans DOS box
@@ -29,15 +27,6 @@ EffectifPhysique = {
 	//Récupére les effectifs Physique présents du mois 	 
 	get_effectifPhysiquePresent: function(in1,fn_cb) {	
   		var q = AgentsMod.using('db');	
-//console.log('get_effectifPhysiquePresent()');
-
-//		q.model(BASE_DONNEES,q.sql('qget_etablis'),fn_cb);  
-
-// console.log( 'select  lib_departement_ep, lib_categorie_ep, count_agent,  Kets,  date_ep '
-//			+ 'from effectif_physique where date_ep >= ' + in1.data.sd + ' and date_ep <= '  + in1.data.ed + ' and kets = ' + in1.data.Kets);
-
-
-//**		var sql = "select lib_departement_ep, lib_categorie_ep, count_agent,  Kets,  date_ep from effectif_physique where ";
 		
 		var sql = "select lib_departement_ep, lib_categorie_ep, count_agent, Kets, date_ep from effectif_physique where date_ep >= '" 
 				+ in1.sd + "' and date_ep <= '" + in1.ed + "' and ";
@@ -50,16 +39,14 @@ EffectifPhysique = {
 			params.push("Ksub=" + in1.Ksub);		
 		sql += params.join('and ');
 		console.log(sql);
-		q.model(BASE_DONNEES, sql, fn_cb);
-
-//			+ 'from effectif_physique where date_ep >= ' + in1.data.sd + ' and date_ep <= '  + in1.data.ed + ' and kets = ' + in1.data.Kets, fn_cb);			
+		q.model(BASE_DONNEES, sql, fn_cb);			
 	},
 
 	
 	//Récupére les grand totaux des effectifs présents du mois 	 
 	get_gdTotalEffPresent: function(in1,fn_cb) {	
   		var q = AgentsMod.using('db');	
-console.log('get_gdTotalEffPresent()');
+		
 		q.model(BASE_DONNEES, 'select lib_departement_ep, sum(count_agent) as GrandTotal '
 			+ 'from effectif_physique where date_ep >= \'2015-07-01\' and date_ep <= \'2015-07-31\' '
 			+ 'group by lib_departement_ep', fn_cb);
