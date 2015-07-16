633db5713bbd9de7ed8bfaa0a3a55d9d2346f2c7
XXX <xxx@xxx.com>
2015-07-16 11:06:29 +0200

    Update# 1812

--- src/Contents/Services/EffectifETP.js
+++ src/Contents/Services/EffectifETP.js
@@ -14,13 +14,11 @@ var BASE_DONNEES = 'bpclight';	//base de données de l'application
 
 
 EffectifETP = {
+	
 	//Calcule les effectifs ETP présents
-//	 cherche: function(in1,fn_cb) {
 	calculate_effectifETP: function(in1,fn_cb) {
-		// 1er argument = error
-		// 2eme argument = response		
+	
 		var q = EffectifETP.using('db');  
-//		console.log('>>EffectifETP.cherche()');
 //		console.log(q.sql('qget_EffectifETP',{RECHERCHE: in1.param_recherche}));	//log apparait dans DOS box
 		q.model('bpclight',q.sql('qget_EffectifETP',{RECHERCHE: in1.param_recherche}),fn_cb);
 	},
@@ -29,10 +27,7 @@ EffectifETP = {
 	//Récupére les effectifs ETP présents du mois 	 
 	get_effectifETPPresent: function(in1,fn_cb) {	
   		var q = AgentsMod.using('db');	
-console.log('get_effectifETPPresent()');
-//oo		q.model(BASE_DONNEES, 'select  lib_departement_etp, lib_categorie_etp, sum_quot,  Kets,  date_etp '
-//oo			+ 'from effectif_etp where date_etp >= \'2015-07-01\' and date_etp <= \'2015-07-31\' ', fn_cb);
-// //////////////////			
+			
 		var sql = "select lib_departement_etp, lib_categorie_etp, sum_quot, Kets, date_etp from effectif_etp where date_etp >= '" 
 				+ in1.sd + "' and date_etp <= '" + in1.ed + "' and ";
 		var params= [];
@@ -44,10 +39,8 @@ console.log('get_effectifETPPresent()');
 			params.push("Ksub=" + in1.Ksub);		
 		sql += params.join('and ');
 		console.log(sql);
-		q.model(BASE_DONNEES, sql, fn_cb);
-			
-			
-			
+		q.model(BASE_DONNEES, sql, fn_cb);		
+						
 			
 //		q.model(BASE_DONNEES,q.sql('qget_etablis'),fn_cb);  
 	},
