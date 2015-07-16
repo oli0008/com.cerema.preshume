b060dfdce1eb72a81c7bc79d3f5355097eca27a2
XXX <xxx@xxx.com>
2015-07-16 11:25:49 +0200

    Update# 1818

--- src/Contents/Services/Service.js
+++ src/Contents/Services/Service.js
@@ -3,13 +3,21 @@
 Service = {
 	get_service: function(in1,fn_cb) {
 		// 1er argument = error
-		// 2eme argument = response		
+		// 2eme argument = response	
+/*		
   		var q = Service.using('db');
 		console.log('>>Service.get_service() --START---');
 		console.log('in1.id_Service= ' + in1.id_Service );
 		console.log(sql( 'qget_service' + {RECHERCHE: in1.id_Service} ) );		//log apparait dans DOS box
 		console.log('>>Service.get_service() --END---');
 		q.model('bpclight',q.sql('qget_service',{RECHERCHE: in1.id_Service}),fn_cb);
+		
+*/	
+		 //ALT
+console.log('>>Service.get_service() --START---');
+console.log('select Ksub, LibSubc, LibSub from subdis where archive = 0 and Kuni = ' + in1.id_Service);
+		q.model('bpclight', 'select Ksub, LibSubc, LibSub from subdis where archive = 0 and Kuni = ' + in1.id_Service, fn_cb);
+console.log('>>Service.get_service() --END---');
 	}
 };
 
