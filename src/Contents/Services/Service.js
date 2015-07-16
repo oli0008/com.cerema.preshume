601cb4a351d4ecf79bc10f11ce874c33017ae64c
XXX <xxx@xxx.com>
2015-07-16 11:32:42 +0200

    Update# 1820

--- src/Contents/Services/Service.js
+++ src/Contents/Services/Service.js
@@ -8,7 +8,7 @@ Service = {
   		var q = Service.using('db');
 console.log('>>Service.get_service() --START---');
 		console.log('in1.id_Service= ' + in1.id_Service );
-		console.log(sql( 'qget_service' + {RECHERCHE:in1.id_Service} ) );		//log apparait dans DOS box
+		console.log(q.sql( 'qget_service' + {RECHERCHE:in1.id_Service} ) );		//log apparait dans DOS box
 console.log('>>Service.get_service() --END---');
 		q.model('bpclight',q.sql('qget_service',{RECHERCHE:in1.id_Service}),fn_cb);
 		
