5b85eae4b6ead6f8a9325978f3650c3af2b28e5c
XXX <xxx@xxx.com>
2015-07-16 12:05:32 +0200

    Update# 1827

--- src/Contents/Services/Service.js
+++ src/Contents/Services/Service.js
@@ -2,13 +2,10 @@
 // class omneedia db = classe moteur d'abstraction de base de données
 Service = {
 	get_service: function(in1,fn_cb) {
-		// 1er argument = error
-		// 2eme argument = response	
 		
   		var q = Service.using('db');
-console.log('>>Service.get_service() --START---');
-		console.log('in1.id_Service= ' + in1.id_Service );
-		console.log(q.sql( 'qget_service' + {RECHERCHE:in1.id_Service} ) );		//log apparait dans DOS box
+
+		console.log(q.sql('qget_service'+{RECHERCHE:in1.id_Service}));		//log apparait dans DOS box
 console.log('>>Service.get_service() --END---');
 		q.model('bpclight',q.sql('qget_service',{RECHERCHE:in1.id_Service}),fn_cb);
 		
